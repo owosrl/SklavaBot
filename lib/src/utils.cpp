4488baab9bbfaffff63a01e4727ea76abd79031a
owosrl <polina@hypercute.eu>
2023-06-01 14:56:59 +0200

    Removed date.h library, timestamp formatted using Boost Library

--- lib/src/utils.cpp
+++ lib/src/utils.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 #include <chrono>
-
-#include "date.h"
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/local_time_adjustor.hpp>
+#include <boost/date_time/c_local_time_adjustor.hpp>
 
 void flush() {
     std::cout << "\n" << std::flush;
@@ -10,9 +11,12 @@ void flush() {
 }
 
 void log_cmd(std::string command, std::string user, std::int32_t timestamp) {
-    date::sys_seconds tp{std::chrono::seconds{timestamp}};
-    std::string time = date::format("%Y-%m-%d %I:%M:%S %p", tp);
+    typedef boost::date_time::c_local_adjustor<boost::posix_time::ptime> local_adj;
 
+
+    std::string time = boost::posix_time::to_simple_string(
+            local_adj::utc_to_local(boost::posix_time::from_time_t(timestamp)
+        ));
 
     std::cout << "[" << command << "] " 
               << "sent by @" << user 
